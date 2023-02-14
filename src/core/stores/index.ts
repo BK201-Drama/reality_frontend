ec90ca0ceba28d3360b6dc577c4f51170931d996
chenyixin@ray003 <chenyixin@rayvision.com>
2023-02-14 21:40:54 +0800

    feat: 添加stores

---
+++ src/core/stores/index.ts
@@ -0,0 +1,14 @@
+import { observer } from 'mobx-react-lite'
+import { createContext, useContext } from 'react'
+
+function createStores () {
+    return {
+
+    }
+}
+
+export const stores = createStores()
+
+export const StoresContext = createContext(stores)
+
+export const useStores = () => useContext(StoresContext)
