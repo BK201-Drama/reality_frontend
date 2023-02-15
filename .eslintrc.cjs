b14902a9bd8e5c95ffc42b5066697df771395d23
chenyixin@ray003 <chenyixin@rayvision.com>
2023-02-15 23:41:26 +0800

    chore: 继续配置prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,11 @@
+/*
+ * @Author: chenyixin@ray003 chenyixin@rayvision.com
+ * @Date: 2023-02-13 23:48:46
+ * @LastEditors: chenyixin@ray003 chenyixin@rayvision.com
+ * @LastEditTime: 2023-02-15 23:37:56
+ * @FilePath: \realty_frontend\.eslintrc.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports = {
   env: {
     browser: true,
@@ -5,7 +13,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:prettier/recommended'
   ],
   overrides: [
   ],
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,21 @@
  * @Author: chenyixin@ray003 chenyixin@rayvision.com
  * @Date: 2023-02-12 22:32:06
  * @LastEditors: chenyixin@ray003 chenyixin@rayvision.com
- * @LastEditTime: 2023-02-14 21:30:31
+ * @LastEditTime: 2023-02-15 23:39:24
  * @FilePath: \realty_frontend\src\main.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
