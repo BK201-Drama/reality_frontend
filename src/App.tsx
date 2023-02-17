c7acc1b7155c5c2505c09d13c63c84db82a95545
chenyixin@ray003 <chenyixin@rayvision.com>
2023-02-17 23:51:51 +0800

    feat: init demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,76 @@
-/*
- * @Author: chenyixin@ray003 chenyixin@rayvision.com
- * @Date: 2023-02-12 22:32:06
- * @LastEditors: chenyixin@ray003 chenyixin@rayvision.com
- * @LastEditTime: 2023-02-14 21:35:25
- * @FilePath: \realty_frontend\src\App.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content, Sider } = Layout;
+
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-[40px]'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: colorBgContainer }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+            items={items2}
+          />
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,26 @@
  * @Author: chenyixin@ray003 chenyixin@rayvision.com
  * @Date: 2023-02-12 22:32:06
  * @LastEditors: chenyixin@ray003 chenyixin@rayvision.com
- * @LastEditTime: 2023-02-15 23:58:50
+ * @LastEditTime: 2023-02-17 23:50:18
  * @FilePath: \realty_frontend\src\main.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { stores, StoresContext } from './core/stores'
-
-import './index.css'
+import { ConfigProvider } from 'antd';
+import 'antd/dist/reset.css';
+import zhCN from 'antd/locale/zh_CN';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { stores, StoresContext } from './core/stores';
+import './index.css';
+import { Button } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StoresContext.Provider value={stores}>
+  <ConfigProvider locale={zhCN}>
+      <StoresContext.Provider value={stores}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StoresContext.Provider>,
+  </StoresContext.Provider>
+  </ConfigProvider>
 )
