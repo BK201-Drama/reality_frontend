40a9a3c12cdc74412388fcff5c2a626bbd46d483
chenyixin@ray003 <chenyixin@rayvision.com>
2023-02-18 14:33:54 +0800

    feat: header

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -11,9 +11,9 @@ const Login = () => {
     navInstance.toWelcome()
   }
   return (
-    <div className="w-[100vw] h-[100vh] bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 flex items-center justify-center">
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-r from-yellow-400 to-pink-500 flex items-center justify-center">
       <div className="w-[500px] h-[500px] bg-[#fff] rounded-[8px] flex justify-center items-between">
-        <div className="p-[20px] flex flex-col gap-[40px] justify-center items-center">
+        <div className="p-[20px] flex flex-col gap-[40px] justify-center items-center backdrop-filter">
           <h1>枫叶公寓</h1>
           <h2>登录</h2>
           <Form form={formInstance} colon={false} onFinish={finishLogin}>
--- src/shared/FHeader/index.tsx
+++ src/shared/FHeader/index.tsx
@@ -1,11 +1,19 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
+import navHelper from '../../core/route/navHelper';
 
 const { Header } = Layout;
 
 const FHeader = () => {
+  const navInstance = navHelper()
   return (
-    <Header className="bg-[#fff]">
-      <div className="" />
+    <Header>
+      <div className='flex items-center h-full justify-between'>
+        <h2 className='text-[#fff]'>枫叶公寓</h2>
+        <Button className='text-[#dee]' onClick={() => {
+          localStorage.removeItem("token")
+          navInstance.toLogin()
+        }}>注销</Button>
+      </div>
     </Header>
   )
 }
