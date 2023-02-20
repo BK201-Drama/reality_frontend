46bb1a518f56408285fddda422824436da1d7d20
chenyixin@ray003 <chenyixin@rayvision.com>
2023-02-21 00:25:40 +0800

    rtable

---
+++ src/shared/RTable/index.tsx
@@ -0,0 +1,36 @@
+import { Table, TableProps } from 'antd'
+import _ from 'lodash';
+import { FC } from 'react';
+
+function RTablePropConfig<T>(tableProps?: TableProps<T>) {
+  return {
+    ...tableProps,
+    pagination: !_.isBoolean(tableProps?.pagination) ? {
+      className: '',
+      showTotal: (total: number) => (
+        <div>
+          共<span>{total}</span>个数据
+        </div>
+      ),
+      showQuickJumper: true,
+      showSizeChanger: true,
+      position: ['bottomCenter'],
+      ...tableProps?.pagination,
+    } : tableProps?.pagination,
+  }
+}
+
+interface ITable {
+  rowKey?: 'string';
+  columns?: any[];
+  tableProps?: any
+}
+const RTable: FC<ITable> = ({ rowKey, columns, tableProps }) => {
+  const dataSource = (RTablePropConfig(tableProps ?? {})?.dataSource ?? []) as any[]
+  const realTableProps = (RTablePropConfig(tableProps ?? {}) ?? {}) as any
+  return (
+    <Table rowKey={rowKey ?? ''} columns={columns ?? []} dataSource={dataSource} {...realTableProps} />
+  )
+}
+
+export default RTable
