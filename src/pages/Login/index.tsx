7f183a8b4b4d78f4c070a1a4909963371697ec72
chenyixin@ray003 <chenyixin@rayvision.com>
2023-02-23 23:14:14 +0800

    feat: add check pwd

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,11 +2,12 @@
  * @Author: chenyixin@ray003 chenyixin@rayvision.com
  * @Date: 2023-02-18 01:22:09
  * @LastEditors: chenyixin@ray003 chenyixin@rayvision.com
- * @LastEditTime: 2023-02-20 23:36:48
+ * @LastEditTime: 2023-02-23 23:13:57
  * @FilePath: \realty_frontend\src\pages\Login\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { Button, Form, Input, message } from 'antd'
+import _ from 'lodash'
 import { useEffect } from 'react'
 import navHelper from '../../core/route/navHelper'
 import { useStores } from '../../core/stores'
@@ -57,6 +58,10 @@ const Login = () => {
         const {
           data: data
         } = res
+        if (_.isNil(data.data)) {
+          message.error('登录失败,检查是否账号密码错误')
+          return
+        }
         message.success('登录成功')
         localStorage.setItem('user', JSON.stringify(data?.data ?? {}))
         userStore.setUserMessage(data?.data ?? {})
