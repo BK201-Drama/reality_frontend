50a5e62cc958311db632f21f33523f4cf16ff973
BK201-Drama <1243532812@qq.com>
2023-03-01 00:25:46 +0800

    feat: add vis

--- src/domains/charge/reposity.ts
+++ src/domains/charge/reposity.ts
@@ -1,3 +1,5 @@
 import { post } from "../../core/services/axios";
 
 export const recharge = (params: any) => post('/user/charge', params)
+
+export const visCharges = (params: any) => post('/user/virtualExpend', params)
